ecb9a56ccd33e8db2d3d69cbe7bbc842290a3f49
Alimjan Mettursun <amettursun@ebay.com>
2019-07-10 10:19:11 +0800

    大迷宫更新，不采用递归方法

--- src/main/java/com/lenkee/app/逃离大迷宫.java
+++ src/main/java/com/lenkee/app/逃离大迷宫.java
@@ -1,7 +1,7 @@
 package com.lenkee.app;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Stack;
 
 /**
  * Created by amettursun on 2019/7/5.
@@ -15,100 +15,21 @@ public class 逃离大迷宫 {
      来源：力扣（LeetCode）
      链接：https://leetcode-cn.com/problems/escape-a-large-maze
      */
+    private static final int scope = 100000; // 地图长宽
+    public static Stack<int[]> trace = new Stack<>();
     public static void main(String[] args) {
-        int[][] blocked = {{1,0},{1,3},{2,1},{2,2}};
+        int[][] blocked = {{0,2},{1,0},{1,3},{2,1},{2,3},{1,0},{1,0},{3,0}};
         int[][] graph = dealData(blocked);
         int[] source = new int[]{0,0};
-        int[] target = new int[]{6,2};
-        
-        System.out.println(isEscapePossible(graph, source, target));
-        for (int i = 0; i < list.size(); i++) {
-            for (int j = 0; j < list.get(i).length; j++) {
-                System.out.print(list.get(i)[j]+" ");
-            }
-            System.out.print("->");
-        }
+        int[] target = new int[]{2,4};
+
+        System.out.println(isEscapePossible2(graph, source, target));
+
     }
-    public static List<int[]> list = new ArrayList<>();
-
-    public static boolean isEscapePossible(int[][] blocked, int[] source, int[] target) {
-        int x = source[0];
-        int y = source[1];
-        // 出口
-        if (x == target[0] && y==target[1]){
-            list.add(new int[]{x,y});
-            return true;
-        }
-
-        // 上下左右移动
-        // 上
-        if (outOfScope(x-1, y,blocked)){
-            blocked[x][y] = 1;  // 把当前的状态block
-            list.add(new int[]{x,y}); // 记录路径
-            // 跳到下一步
-            source[0] = x-1;
-            source[1] = y;
-            if (!isEscapePossible(blocked, source, target)){
-                // 跳回来
-                list.remove(list.size()-1); //删除路径
-                source[0] = x;
-                source[1] = y;
-            }
-        }
-        // 下
-        if (outOfScope(x+1, y,blocked)){
-            blocked[x][y] = 1;  // 把当前的状态block
-            list.add(new int[]{x,y}); // 记录路径
-            // 跳到下一步
-            source[0] = x+1;
-            source[1] = y;
-            if (!isEscapePossible(blocked, source, target)){
-                // 跳回来
-                list.remove(list.size()-1); //删除路径
-                source[0] = x;
-                source[1] = y;
-            }else
-                return true;
-        }
-        // 左边
-
-        if (outOfScope(x, y-1,blocked)){
-            blocked[x][y] = 1;  // 把当前的状态block
-            list.add(new int[]{x,y}); // 记录路径
-            // 跳到下一步
-            source[0] = x;
-            source[1] = y-1;
-            if (!isEscapePossible(blocked, source, target)){
-                list.remove(list.size()-1); //删除路径
-                // 跳回来
-                source[0] = x;
-                source[1] = y;
-            }else
-                return true;
-        }
-        // 右边
-
-        if (outOfScope(x, y+1,blocked)){
-            blocked[x][y] = 1;  // 把当前的状态block
-            list.add(new int[]{x,y}); // 记录路径
-            // 跳到下一步
-            source[0] = x;
-            source[1] = y+1;
-            if (!isEscapePossible(blocked, source, target)){
-                list.remove(list.size()-1); //删除路径
-                // 跳回来
-                source[0] = x;
-                source[1] = y;
-            }else
-                return true;
-        }
 
 
-        return false;
-    }
-    
     public static int[][] dealData(int[][]blocked){
-        int[][] graph = new int[10][10];
+        int[][] graph = new int[scope][scope];
         for (int i = 0; i < blocked.length; i++) {
             int x = blocked[i][0];
             int y = blocked[i][1];
@@ -121,4 +42,107 @@ public class 逃离大迷宫 {
         return x>=0 && y>=0 && x<blocked.length && y<blocked[0].length && blocked[x][y]==0;
     }
 
+    public static boolean isEscapePossible2(int[][] graph, int[] source, int[] target){
+        // 上下左右轮询，一直到taget和source是同一个点为止
+        trace.add(source.clone());
+        while (true){
+            int x = source[0];
+            int y = source[1];
+            System.out.print(x+" "+y+"-->");
+            // 出口
+            if (x == target[0] && y==target[1]){
+                return true;
+            }
+            // 上
+            if (goAhead(graph,source, 1)){
+                // 移动成功，将路径加入到stack里面, 移动source
+                trace.add(source.clone());
+                continue;
+            }else if (goAhead(graph,source, 2)){
+                // 下
+                trace.add(source.clone());
+                continue;
+            }else if (goAhead(graph,source, 3)){
+                // 左
+                trace.add(source.clone());
+                continue;
+            }else if (goAhead(graph,source, 4)){
+                // 右
+                trace.add(source.clone());
+                continue;
+            }else {
+                // 失败，回撤
+                int[] ints = rollBack(trace, graph);
+                if (ints != null)
+                    source = ints.clone();
+                else
+                    source = null;
+            }
+            // 退回到起点, 走投无路
+            if (trace.size() ==0 && source == null){
+                return false;
+            }
+
+        }
+
+    }
+
+    // 如果能走，则走到下一步，返回true，否则返回false
+    public static boolean goAhead(int[][]graph,int[]source, int director){
+        // 1.上 2.下 3.左 4.右
+        int x = source[0];
+        int y = source[1];
+        switch (director){
+            case 1:{
+                if (outOfScope(x-1, y,graph)){
+                    graph[x][y] = 1;
+                    source[0] = x-1;
+                    source[1] = y;
+                    return true;
+                }else
+                    return false;
+            }
+            case 2:{
+                if (outOfScope(x+1, y,graph)){
+                    graph[x][y] = 1;
+                    source[0] = x+1;
+                    source[1] = y;
+                    return true;
+                }else
+                    return false;
+            }
+            case 3:{
+                if (outOfScope(x, y-1,graph)){
+                    graph[x][y] = 1;
+                    source[0] = x;
+                    source[1] = y-1;
+                    return true;
+                }else
+                    return false;
+            }
+            case 4:{
+                if (outOfScope(x, y+1,graph)){
+                    graph[x][y] = 1;
+                    source[0] = x;
+                    source[1] = y+1;
+                    return true;
+                }else
+                    return false;
+            }
+            default:return false;
+        }
+    }
+
+    // 返回source位置
+    public static int[] rollBack(Stack<int[]> stack, int[][]graph){
+        try {
+            int[] pop = stack.pop();
+            int x = pop[0];
+            int y = pop[1];
+            graph[x][y] = 1;
+            return stack.lastElement();
+        }catch (NoSuchElementException e){
+            return null;
+        }
+    }
 }
